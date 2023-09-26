8b74121aff8e17ede97515563157b38659225648
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2023-09-26 15:49:53 -0300

    fix quake data importer

--- scripts/import_quake_data.rb
+++ scripts/import_quake_data.rb
@@ -3,37 +3,7 @@ require 'pg'
 require_relative '../db/connect'
 
 class QuakeDataImporter
-  METHOD_MAP = {
-    '0' => 'MOD_UNKNOWN',
-    '1' => 'MOD_SHOTGUN',
-    '2' => 'MOD_GAUNTLET',
-    '3' => 'MOD_MACHINEGUN',
-    '4' => 'MOD_GRENADE',
-    '5' => 'MOD_GRENADE_SPLASH',
-    '6' => 'MOD_ROCKET',
-    '7' => 'MOD_ROCKET_SPLASH',
-    '8' => 'MOD_PLASMA',
-    '9' => 'MOD_PLASMA_SPLASH',
-    '10' => 'MOD_RAILGUN',
-    '11' => 'MOD_LIGHTNING',
-    '12' => 'MOD_BFG',
-    '13' => 'MOD_BFG_SPLASH',
-    '14' => 'MOD_WATER',
-    '15' => 'MOD_SLIME',
-    '16' => 'MOD_LAVA',
-    '17' => 'MOD_CRUSH',
-    '18' => 'MOD_TELEFRAG',
-    '19' => 'MOD_FALLING',
-    '20' => 'MOD_SUICIDE',
-    '21' => 'MOD_TARGET_LASER',
-    '22' => 'MOD_TRIGGER_HURT',
-    '23' => 'MOD_NAIL',
-    '24' => 'MOD_CHAINGUN',
-    '25' => 'MOD_PROXIMITY_MINE',
-    '26' => 'MOD_KAMIKAZE',
-    '27' => 'MOD_JUICED',
-    '28' => 'MOD_GRAPPLE'
-  }  
+  KILL_LOG_REGEX = /(\d+:\d+) Kill: \d+ \d+ \d+: (.+?) killed (.+?) by (MOD_\w+)/
 
   def self.download_data
     url = "https://gist.githubusercontent.com/cloudwalk-tests/be1b636e58abff14088c8b5309f575d8/raw/"
@@ -41,35 +11,28 @@ class QuakeDataImporter
   end
 
   def self.import
-    data = download_data
     games = []
     current_game = nil
-    method_name = ''
-  
+
     data.each_line do |line|
       case line
-      when /InitGame/
-        current_game = { total_kills: 0, players: [], kills: {} }
-      when /ShutdownGame/
-        games << current_game if current_game
-        current_game = nil
+      when /InitGame:/
+        current_game = { total_kills: 0, players: [], kills: [] }
+      when /ShutdownGame:/
+        games << current_game
       when /Kill:/
         current_game[:total_kills] += 1
-      
-        _, killer, victim, method = line.split
-        method_name = METHOD_MAP[method] || 'UNKNOWN'
-        killer_name = killer.split('<').last.split('>').first
-        victim_name = victim.split('<').last.split('>').first
-      
-        current_game[:kills][killer_name] ||= 0
-        current_game[:kills][killer_name] += 1 unless killer_name == "<world>"
-        current_game[:kills][victim_name] ||= 0
-        current_game[:kills][victim_name] -= 1 if killer_name == "<world>"
-      
-        current_game[:players] << killer_name unless current_game[:players].include?(killer_name) || killer_name == "<world>"
-        current_game[:players] << victim_name unless current_game[:players].include?(victim_name) || victim_name == "<world>"
+
+        _, killer, victim, method = line.scan(KILL_LOG_REGEX)[0]
+
+        current_game[:kills] << { killer: killer, victim: victim, method: method }
+
+        current_game[:players] << killer unless current_game[:players].include?(killer) || killer == "<world>"
+        current_game[:players] << victim unless current_game[:players].include?(victim)
       end
     end
+
+    games.select! { _1[:total_kills] > 0 }
 
     conn = connect_to_db
 
@@ -78,18 +41,20 @@ class QuakeDataImporter
     conn.prepare('insert_kill', "INSERT INTO kills (game_id, killer_id, victim_id, method) VALUES ($1, $2, $3, $4)")
 
     games.each do |game|
-      game_id = conn.exec_prepared('insert_game', [game[:total_kills]])[0]['id'].to_i
+      total_kills = game[:total_kills].to_i
+      game_id = conn.exec_prepared('insert_game', [total_kills])[0]['id'].to_i
 
       game[:players].each do |player_name|
         conn.exec_prepared('insert_player', [player_name])
       end
 
-      game[:kills].each do |player_name, kills_count|
-        player_id = conn.exec("SELECT id FROM players WHERE name = '#{player_name}'")[0]['id'].to_i
-        kills_count.times do
-          conn.exec_prepared('insert_kill', [game_id, player_id, method_name])
-        end
-      end      
+      game[:kills].each do |kill|
+        killer_id = find_player_id_by_name(conn, kill[:killer])
+        victim_id = find_player_id_by_name(conn, kill[:victim])
+        method = kill[:method]
+
+        conn.exec_prepared('insert_kill', [game_id, killer_id, victim_id, method])
+      end
     end
 
     conn.exec("DEALLOCATE insert_game")
@@ -97,5 +62,17 @@ class QuakeDataImporter
     conn.exec("DEALLOCATE insert_kill")
 
     conn.close
+  end
+
+  private
+
+  def self.data
+    @data_cache ||= download_data
+  end
+
+  def self.find_player_id_by_name(conn, name)
+    return nil if name == '<world>'
+
+    conn.exec("SELECT id FROM players WHERE name = '#{name}'")[0]['id'].to_i
   end
 end
