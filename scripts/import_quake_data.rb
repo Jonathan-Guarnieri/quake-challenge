2057899bb5500bdee936974b9c2bbb1e808ead3a
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2023-09-26 16:14:41 -0300

    improve importer and specs

--- scripts/import_quake_data.rb
+++ scripts/import_quake_data.rb
@@ -5,12 +5,10 @@ require_relative '../db/connect'
 class QuakeDataImporter
   KILL_LOG_REGEX = /(\d+:\d+) Kill: \d+ \d+ \d+: (.+?) killed (.+?) by (MOD_\w+)/
 
-  def self.download_data
-    url = "https://gist.githubusercontent.com/cloudwalk-tests/be1b636e58abff14088c8b5309f575d8/raw/"
-    Net::HTTP.get(URI(url))
-  end
-
   def self.import
+    conn = connect_to_db
+    return if games_already_imported?
+
     games = []
     current_game = nil
 
@@ -32,10 +30,6 @@ class QuakeDataImporter
       end
     end
 
-    games.select! { _1[:total_kills] > 0 }
-
-    conn = connect_to_db
-
     conn.prepare('insert_game', "INSERT INTO games (total_kills) VALUES ($1) RETURNING id")
     conn.prepare('insert_player', "INSERT INTO players (name) VALUES ($1) ON CONFLICT (name) DO NOTHING RETURNING id")
     conn.prepare('insert_kill', "INSERT INTO kills (game_id, killer_id, victim_id, method) VALUES ($1, $2, $3, $4)")
@@ -49,8 +43,8 @@ class QuakeDataImporter
       end
 
       game[:kills].each do |kill|
-        killer_id = find_player_id_by_name(conn, kill[:killer])
-        victim_id = find_player_id_by_name(conn, kill[:victim])
+        killer_id = find_player_id_by_name(kill[:killer])
+        victim_id = find_player_id_by_name(kill[:victim])
         method = kill[:method]
 
         conn.exec_prepared('insert_kill', [game_id, killer_id, victim_id, method])
@@ -66,13 +60,27 @@ class QuakeDataImporter
 
   private
 
+  def self.download_data
+    url = "https://gist.githubusercontent.com/cloudwalk-tests/be1b636e58abff14088c8b5309f575d8/raw/"
+    Net::HTTP.get(URI(url))
+  end
+
   def self.data
     @data_cache ||= download_data
   end
 
-  def self.find_player_id_by_name(conn, name)
+  def self.find_player_id_by_name(name)
     return nil if name == '<world>'
 
     conn.exec("SELECT id FROM players WHERE name = '#{name}'")[0]['id'].to_i
+  end
+
+  def self.games_already_imported?
+    games_count = conn.exec("SELECT COUNT(*) FROM games")[0]['count'].to_i
+    games_count.positive?
+  end
+
+  def self.conn
+    @conn ||= connect_to_db
   end
 end
--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -2,30 +2,99 @@ require_relative '../db/connect'
 require_relative '../scripts/import_quake_data'
 
 RSpec.describe QuakeDataImporter do
-  before(:all) do
-    conn = connect_to_db
+  let(:conn) { connect_to_db }
+
+  before(:each) do
+    described_class.import
+  end
+
+  after(:each) do
     conn.exec("DELETE FROM kills;")
     conn.exec("DELETE FROM players;")
     conn.exec("DELETE FROM games;")
-    conn.close
   end
 
   describe ".import" do
-    it "imports data correctly" do
-      described_class.import
-
-      conn = connect_to_db
-
+    it "imports the correct number of games" do
       games_count = conn.exec("SELECT COUNT(*) FROM games")[0]['count'].to_i
-      expect(games_count).to be > 0
-
-      players_count = conn.exec("SELECT COUNT(*) FROM players")[0]['count'].to_i
-      expect(players_count).to be > 0
-
-      kills_count = conn.exec("SELECT COUNT(*) FROM kills")[0]['count'].to_i
-      expect(kills_count).to be > 0
-
-      conn.close
+      expect(games_count).to eq(expected_games_count)
     end
+
+    it "imports the correct number of players" do
+      players_count = conn.exec("SELECT COUNT(*) FROM players")[0]['count'].to_i
+      expect(players_count).to eq(expected_players_count)
+    end
+
+    it "imports the correct number of kills" do
+      kills_count = conn.exec("SELECT COUNT(*) FROM kills")[0]['count'].to_i
+      expect(kills_count).to eq(expected_kills_count)
+    end
+
+    it "imports specific players correctly" do
+      player = conn.exec("SELECT name FROM players WHERE name = 'Isgalamido'")[0]
+      expect(player).not_to be_nil
+    end
+
+    it "does not include <world> as a player" do
+      world_player = conn.exec("SELECT COUNT(*) FROM players WHERE name = '<world>'")[0]['count'].to_i
+      expect(world_player).to eq(0)
+    end
+
+    it "registers world kills correctly" do
+      world_kills = conn.exec("SELECT COUNT(*) FROM kills WHERE killer_id IS NULL")[0]['count'].to_i
+      expect(world_kills).to eq(expected_world_kills)
+    end
+
+    it "verifies game without kills" do
+      game_with_no_kills = conn.exec("SELECT COUNT(*) FROM games WHERE total_kills = 0")[0]['count'].to_i
+      expect(game_with_no_kills).to eq(expected_games_without_kills)
+    end
+
+    it "ensures total kills consistency" do
+      games = conn.exec("SELECT * FROM games")
+      games.each do |game|
+        total_kills = game["total_kills"].to_i
+        individual_kills = conn.exec("SELECT COUNT(*) FROM kills WHERE game_id = #{game["id"]}")[0]['count'].to_i
+        expect(total_kills).to eq(individual_kills)
+      end
+    end
+
+    it "does not duplicate data on subsequent imports" do
+      initial_games_count = conn.exec("SELECT COUNT(*) FROM games")[0]['count'].to_i
+      described_class.import
+      second_games_count = conn.exec("SELECT COUNT(*) FROM games")[0]['count'].to_i
+      expect(initial_games_count).to eq(second_games_count)
+    end
+
+    it "imports kill methods correctly" do
+      mod_railgun_kills = conn.exec("SELECT COUNT(*) FROM kills WHERE method = 'MOD_RAILGUN'")[0]['count'].to_i
+      expect(mod_railgun_kills).to eq(expected_mod_railgun_kills)
+    end
+  end
+
+  # Add helper methods or constants:
+
+  def expected_games_count
+    20
+  end
+
+  def expected_players_count
+    10
+  end
+
+  def expected_kills_count
+    1058
+  end
+
+  def expected_world_kills
+    232
+  end
+
+  def expected_mod_railgun_kills
+    132
+  end
+
+  def expected_games_without_kills
+    2
   end
 end
